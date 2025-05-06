acdd48f0c83677648365205bd4d48402df8b2e62
clmb <cibence88@gmail.com>
2025-05-05 23:50:40 -0400

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -1,3 +1,10 @@
+
+from flask import Flask, request, jsonify, render_template_string, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+import json
+import random
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
